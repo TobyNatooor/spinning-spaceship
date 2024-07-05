88006f05ae364dbaec5bcbb00451aad1b854e123
TobyNatooor <tobiasnf@gmail.com>
2024-07-05 22:53:15 +0200

    added a linkedlist of walls and a function to move them

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "include/raylib.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 #if defined(PLATFORM_WEB)
@@ -23,18 +24,34 @@ int main(void) {
   CloseWindow();
 
   free(loopArg->player);
+  free(loopArg->wallList);
   free(loopArg);
 
   return 0;
 }
 
 struct LoopArg *Setup(void) {
+  // try without allocating
   struct Player *player = malloc(sizeof(struct Player));
   player->position.x = SCREEN_WIDTH / 2;
   player->position.y = SCREEN_HEIGHT / 2;
 
+  struct WallList wall1 = {
+      .wallStart = (Vector2){300, 0},
+      .wallEnd = (Vector2){300, 450},
+      .next = NULL,
+  };
+  struct WallList wall2 = {
+      .wallStart = (Vector2){500, 0},
+      .wallEnd = (Vector2){500, 450},
+      .next = NULL,
+  };
+  struct WallList *wallList = AddWall(NULL, &wall1);
+  wallList = AddWall(wallList, &wall2);
+
   struct LoopArg *loopArg = malloc(sizeof(struct LoopArg));
   loopArg->player = player;
+  loopArg->wallList = wallList;
 
   return loopArg;
 }
@@ -42,6 +59,7 @@ struct LoopArg *Setup(void) {
 void Loop(void *loopArg_) {
   struct LoopArg *arg = loopArg_;
   struct Player *player = arg->player;
+  struct WallList *wallList = arg->wallList;
 
   float frameTime = GetFrameTime();
 
@@ -54,9 +72,57 @@ void Loop(void *loopArg_) {
   if (IsKeyDown(KEY_S))
     player->position.y += 100 * frameTime;
 
+  MoveWallsDown(wallList, 100 * frameTime);
+
+  if (CheckCollisionPlayerWallList(player, wallList))
+    printf("hit wall\n");
+
   BeginDrawing();
 
+  DrawWallList(wallList);
   DrawCircle(player->position.x, player->position.y, 50, BLACK);
 
   EndDrawing();
+}
+
+struct WallList *AddWall(struct WallList *head, struct WallList *newWall) {
+  struct WallList *start = head;
+
+  struct WallList *wallList = malloc(sizeof(struct WallList));
+  *wallList = *newWall;
+
+  if (head == NULL) {
+    return wallList;
+  }
+  while (head->next != NULL) {
+    head = head->next;
+  }
+  head->next = wallList;
+  return start;
+}
+
+void MoveWallsDown(struct WallList *head, int offset) {
+  while (head != NULL) {
+    head->wallStart.y += offset;
+    head->wallEnd.y += offset;
+    head = head->next;
+  }
+}
+
+void DrawWallList(struct WallList *head) {
+  while (head != NULL) {
+    DrawLineV(head->wallStart, head->wallEnd, BLACK);
+    head = head->next;
+  }
+}
+
+bool CheckCollisionPlayerWallList(struct Player *player,
+                                  struct WallList *head) {
+  while (head != NULL) {
+    if (CheckCollisionCircleLine(player->position, 50, head->wallStart,
+                                 head->wallEnd))
+      return true;
+    head = head->next;
+  }
+  return false;
 }
--- src/main.h
+++ src/main.h
@@ -3,12 +3,23 @@
 #define SCREEN_HEIGHT 450
 #define SCREEN_WIDTH 800
 
+struct WallList {
+  struct WallList *next;
+  Vector2 wallStart;
+  Vector2 wallEnd;
+};
 struct Player {
   Vector2 position;
 };
 struct LoopArg {
   struct Player *player;
+  struct WallList *wallList;
 };
 
 struct LoopArg *Setup(void);
 void Loop(void *loopArg);
+
+struct WallList* AddWall(struct WallList *head, struct WallList *newWall);
+void MoveWallsDown(struct WallList *head, int offset);
+void DrawWallList(struct WallList *head);
+bool CheckCollisionPlayerWallList(struct Player *player, struct WallList *head);
